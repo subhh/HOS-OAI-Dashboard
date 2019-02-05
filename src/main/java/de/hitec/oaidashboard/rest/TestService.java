685127ececc8f86fe96832d60c19b4e9014d3d76
Pascal <rost@informatik.uni-hamburg.de>
2019-02-05 19:00:28 +0100

    metha in docker and metha-fortune in rest for first testing

--- src/main/java/de/hitec/oaidashboard/rest/TestService.java
+++ src/main/java/de/hitec/oaidashboard/rest/TestService.java
@@ -1,15 +1,42 @@
-package hitec.oaidashboard.rest;
+package de.hitec.oaidashboard.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Path("/testservice")
 public class TestService {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getTestService() {
-        return "Hello from HITeC rest interface (get) test2";
+    public String getTestService() throws IOException {
+        Runtime rt = Runtime.getRuntime();
+        String[] commands = {"metha-fortune"};
+        Process proc = rt.exec(commands);
+
+        BufferedReader stdInput = new BufferedReader(new
+                InputStreamReader(proc.getInputStream()));
+
+        BufferedReader stdError = new BufferedReader(new
+                InputStreamReader(proc.getErrorStream()));
+
+        // read the output from the command
+        System.out.println("Here is the standard output of the command:\n");
+        String s = null;
+        StringBuilder builder = new StringBuilder();
+        while ((s = stdInput.readLine()) != null) {
+            builder.append(s);
+        }
+
+        // read any errors from the attempted command
+        System.out.println("Here is the standard error of the command (if any):\n");
+        //while ((s = stdError.readLine()) != null) {
+        //    builder.append(s);
+        //}
+        return builder.toString();
+        //return "Hello from HITeC rest interface (get) test2";
     }
 }

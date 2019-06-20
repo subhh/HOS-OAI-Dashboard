cae95f05d7806c5eddaf6cd696dcb5506d6342a9
Rainer Herzog <herzog@informatik.uni-hamburg.de>
2019-06-20 16:12:43 +0200

    added JUnit Tests

---
+++ oai-dashboard_harvester/src/main/java/de/uni_hamburg/sub/oaidashboard/test/AllTests.java
@@ -0,0 +1,19 @@
+//package de.uni_hamburg.sub.oaidashboard.test;
+//
+//import junit.framework.Test;
+//import junit.framework.TestSuite;
+//
+//public class AllTests {
+//	public static Test suite() 
+//	{
+//		TestSuite suite = new TestSuite("Scenior");
+//		
+//		suite.addTestSuite(DSMainTest.class);
+//		suite.addTestSuite(TemporalConstraintNetTest.class);
+//		suite.addTestSuite(ControlTest.class);
+//		suite.addTestSuite(BCHTest.class);
+//		suite.addTestSuite(OntologyAccTest.class);
+//		return suite;
+//	}
+//}
+//
